a6d95210310385635b9cb3a986b6e1d107db4524
ak8051 <ak8051651@gmail.com>
2024-07-27 23:06:59 +0300

    finish Some methods and add some styles edits

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,10 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 
 const Menu = () => {
+  // Data Holder
   const [products, setProducts] = useState([
-    { id: 1, name: "Cookies", count: 5, order: false },
-    { id: 2, name: "Cola", count: 2, order: false },
+    { id: 1, name: "Cookies", price: 50, count: 5, order: false },
+    { id: 2, name: "Cola", price: 30, count: 2, order: false },
   ]);
+
+  // Methods
+  const incFun = (productId) => {
+    const updateProducts = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, count: product.count + 1 };
+      } else {
+        return product;
+      }
+    });
+    setProducts(updateProducts);
+  };
+
+  const orderFood = (productId) => {
+    const updateState = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, order: !product.order };
+      } else {
+        return product;
+      }
+    });
+    setProducts(updateState);
+  };
+
   return (
     <div className="container mt-5">
       <h1>Menu</h1>
@@ -17,13 +42,20 @@ const Menu = () => {
             <th colSpan={2}>Price</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="table-group-divider">
           {products.map((product) => (
             <tr key={product.id}>
               <td>{product.name}</td>
-              <td>{product.count}</td>
+              <td>{product.price}$</td>
               <td>
-                <FontAwesomeIcon icon={faCartPlus} />
+                <FontAwesomeIcon
+                  onClick={() => orderFood(product.id)}
+                  icon={faCartPlus}
+                  style={{
+                    color: product.order ? "black" : "gray",
+                    fontSize: "25px",
+                  }}
+                />
               </td>
             </tr>
           ))}
